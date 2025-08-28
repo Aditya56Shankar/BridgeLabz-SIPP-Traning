c895f4f845420f14069fa9d324ce48c811a61bf2
Aditya56Shankar <122800149+Aditya56Shankar@users.noreply.github.com>
2025-08-28 12:29:13 +0530

    Remove Regex_Junit folder

--- Regex_Junit/ReplaceSpaces.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-public class ReplaceSpaces {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String text = sc.nextLine();
-        System.out.println(text.replaceAll("\\s+", " "));
-    }
-}
--- Regex_Junit/ValidateHexColor.java
+++
@@ -1,9 +0,0 @@
-import java.util.regex.*;
-import java.util.Scanner;
-public class ValidateHexColor {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String color = sc.nextLine();
-        System.out.println(color.matches("^#[0-9A-Fa-f]{6}$"));
-    }
-}
